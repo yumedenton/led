64e2884d9630112d6f3554cf18699efd6257b9eb
yumedenton <47803171+yumedenton@users.noreply.github.com>
2019-02-27 09:28:02 -0500

    Update led1.c

--- led1.c
+++ led1.c
@@ -39,9 +39,9 @@ void loop() {
       // Set the first led back to black for 1 second
       leds[0] = CRGB::Black;
       FastLED.show();
-      delay(500);
-    void loop() {
-        for(int dot = 0; dot < NUM_LEDS; dot++) make { 
+      delay(500)
+        
+    for(int dot = 0; dot < NUM_LEDS; dot++) make { 
             leds[dot] = CRGB::Blue;
             FastLED.show();
             // clear this led for the next time around the loop
