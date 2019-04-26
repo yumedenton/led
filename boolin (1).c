c3a699e86934f365e07482380d3a427792b7cc90
yumedenton <47803171+yumedenton@users.noreply.github.com>
2019-04-26 08:54:02 -0400

    Update boolin (1).c

--- boolin (1).c
+++ boolin (1).c
@@ -4,14 +4,14 @@
 
 ArdusatSerial serialConnection(SERIAL_MODE_HARDWARE_AND_SOFTWARE, 8, 9);
 
-Luminosity lum;
+
 TemperatureMLX temp;
 
 void setup(void)
 {
   serialConnection.begin(9600);
 
-  lum.begin();
+
   temp.begin();
 
   serialConnection.println("");
@@ -20,7 +20,20 @@ void setup(void)
 void loop(void)
 {
   serialConnection.println(temp.readToJSON("infraredTemp"));
-  serialConnection.println(lum.readToJSON("luminosity"));
+
 
   delay(200);
 }
+void loop() {
+ 
+  display.clearDisplay(); 
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  // Fills the display buffer with text
+    {
+    if(temp>=34)
+  display.println("Hot");
+  // Show the display buffer on the OLED display
+  display.display();
+    }
